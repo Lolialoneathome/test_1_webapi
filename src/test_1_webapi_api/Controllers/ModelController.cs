cea440234176c9e0fc2944d8fa1f75d7eebb4679
HP <HP@HP->
2017-07-22 16:30:32 +0500

    TEntity to TModel

--- src/test_1_webapi_api/Controllers/ModelController.cs
+++ src/test_1_webapi_api/Controllers/ModelController.cs
@@ -7,12 +7,12 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace test_1_webapi_api.Controllers
 {
-    public abstract class ModelController<TEntity> : Controller
-        where TEntity : IModel
+    public abstract class ModelController<TModel> : Controller
+        where TModel : IModel
     {
-        protected readonly IRepository<TEntity> _repository;
+        protected readonly IRepository<TModel> _repository;
 
-        public ModelController(IRepository<TEntity> repository)
+        public ModelController(IRepository<TModel> repository)
         {
             if (repository == null)
                 throw new ArgumentNullException(nameof(repository));
@@ -21,14 +21,14 @@ namespace test_1_webapi_api.Controllers
         }
         // GET api/~
         [HttpGet]
-        public IEnumerable<TEntity> Get()
+        public IEnumerable<TModel> Get()
         {
             return _repository.All();
         }
 
         // GET api/~/5
         [HttpGet("{id}")]
-        public TEntity Get(int id)
+        public TModel Get(int id)
         {
             return _repository.GetById(id);
         }
